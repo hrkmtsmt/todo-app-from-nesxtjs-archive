b554a71022f6331bdee2f3eb357891cc801c2b4f
hrkmtsmt <hrkmtsmt@icloud.com>
2022-03-26 19:47:37 +0900

    :bug: [Bug Fix]: Fix component AddTodo

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -4,24 +4,32 @@ import AddIcon from '@mui/icons-material/Add';
 
 type Props = {
   onClickAdd: React.MouseEventHandler<HTMLButtonElement>;
-  onChangeInput: React.FormEventHandler<HTMLFormElement>;
+  onChangeTitle: React.FormEventHandler<HTMLTextAreaElement | HTMLInputElement>;
+  onChangeDetails: React.FormEventHandler<
+    HTMLTextAreaElement | HTMLInputElement
+  >;
 };
 
 export const AddTodo: React.VFC<Props> = (props) => {
-  const { onClickAdd, onChangeInput } = props;
+  const { onClickAdd, onChangeTitle, onChangeDetails } = props;
 
   return (
     <Box
       sx={{ p: '8px 16px', display: 'flex', justifyContent: 'space-between' }}
       component={'form'}
-      onChange={onChangeInput}
     >
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <TextField required label={'Todo'} sx={{ width: '100%' }} />
+          <TextField
+            onChange={onChangeTitle}
+            required
+            label={'Todo'}
+            sx={{ width: '100%' }}
+          />
         </Grid>
         <Grid item xs={12}>
           <TextField
+            onChange={onChangeDetails}
             required
             label={'Details'}
             rows={4}
